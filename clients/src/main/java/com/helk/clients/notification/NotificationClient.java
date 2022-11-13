e6667e2c312ea43618db2d3fd8bf98600ffff709
Hamza EL KHATIB <hamza.elkhatib@protonmail.com>
2022-11-13 01:28:14 +0100

    fix: Fixing Feign client in notification service

--- clients/src/main/java/com/helk/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/helk/clients/notification/NotificationClient.java
@@ -4,8 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(
-        name = "notification",
-        url = "${clients.notification.url}"
+        name = "notification"
 )
 public interface NotificationClient {
 
